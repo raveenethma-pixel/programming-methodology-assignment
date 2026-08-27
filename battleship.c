55082608b3ac0632df656b69270dabfd085b8890
raveenethma-pixel <ravee.nethma@gmail.com>
2026-08-27 08:04:46 +0530

    User input  validation for battleship types

--- battleship.c
+++ battleship.c
@@ -4,4 +4,10 @@ void initializeBattleship(Battleship *B){
     //Initialize the battleship with default values
     printf("Enter the battleship type (U/M/R/S): ");
     scanf(" %c",&B->type);//This is the same as (*B).type, but the arrow operator is much better when dealing with pointers to structures
+    //make sure the input is valid
+    if(B->type!='U' && B->type!='M' && B->type!='R' && B->type!='S'){ 
+        printf("Invalid battleship type. It must be U, M, R, or S.\n");
+        return;
+    }
+    printf("Battleship type %c selected.\n",B->type);//B->type is the same as (*B).type, it points to the chosen type of the battleship
 }
